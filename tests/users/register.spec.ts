63621d4bcee91dbdc87070253a3be424cc3e7e63
ShaikhAnsarAhmed <ansar.ahmed@mokshasolutions.com>
2024-07-26 11:37:35 +0530

    add email sanitization

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -146,4 +146,23 @@ describe("POST auth/register", () => {
       expect(users).toHaveLength(0);
     });
   });
+  describe("fields are not in proper format", () => {
+    it("should trim the email field", async () => {
+      // Arrange
+      const userData = {
+        firstName: "Ansar",
+        lastName: "Shaikh",
+        email: " ansarahmedn@gmail.com ",
+        password: "secret",
+      };
+
+      // Act
+      await request(app).post("/auth/register").send(userData);
+      // Assert
+      const userRepository = connection.getRepository(User);
+      const users = await userRepository.find();
+      const user = users[0];
+      expect(user.email).toBe("ansarahmedn@gmail.com");
+    });
+  });
 });
